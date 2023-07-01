cf7f98a15c192715db346c090475d0c7142c7178
micuch11 <micuch1126@gmail.com>
2023-07-01 20:41:44 +0100

    Async / Dev Tools / Project 10 Assignment

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,25 @@
+function Call_Loop() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
+
+function String_Length() {
+    var text = "Hellow World!";
+    var length = text.length;
+    document.getElementById("Length").innerHTML = length;
+}
+
+var Instruments = ["Guitars", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
